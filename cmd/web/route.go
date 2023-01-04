14ebb0a681f0022008c37a4558611b31911454f3
Mohamad J. Makki <malanbaki@transforat.com>
2023-01-04 10:54:13 +0300

    adding the route package

--- cmd/web/route.go
+++ cmd/web/route.go
@@ -13,7 +13,7 @@ func route(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
-	mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
 
 	mux.Get("/", handler.Repo.Home)
 	mux.Get("/about", handler.Repo.About)
