f1f3366c54dbe80c9186a56b1a6177e5b3962ad5
Mohamad J. Makki <malanbaki@transforat.com>
2023-01-04 10:11:00 +0300

    adding the route package

--- cmd/web/route.go
+++ cmd/web/route.go
@@ -4,12 +4,15 @@ import (
 	"GoWebMux/pkg/config"
 	"GoWebMux/pkg/handler"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func route(app *config.AppConfig) http.Handler {
 
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", handler.Repo.Home)
 	mux.Get("/about", handler.Repo.About)
